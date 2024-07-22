3f165d30a102ac077df99499120602f68d07e59a
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2024-07-22 14:30:50 -0700

    Update Program.cs

--- MsiFicCallWebApi/Program.cs
+++ MsiFicCallWebApi/Program.cs
@@ -14,7 +14,10 @@ namespace ConfidentialClientApp
         private static string tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47"; // MSFT Tenant ID
         private static string clientId = "f62c5ae3-bf3a-4af5-afa8-a68b800396e9"; // RequestMSIDLAB app id
         private static string authority = $"https://login.microsoftonline.com/{tenantId}";
+
+        //Cert location : https://ms.portal.azure.com/#@microsoft.onmicrosoft.com/asset/Microsoft_Azure_KeyVault/Certificate/https://msidlabs.vault.azure.net/certificates/LabAuth
         private static string certificateThumbprint = "f6456f3273677dba268bce224fb6589c5e8fbea2"; // LabAuth cert thumbprint
+        
         private static string apiUrl = "https://service.msidlab.com/environmentvariables?resource=WebApp";
         private static string resourceUrl = "https://management.azure.com"; // The resource you want to access with the managed identity
 
